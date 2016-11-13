61f8410889d946c425a18c2679793bc88e89c36f
Tanay Choudhary <tanay@u.northwestern.edu>
2016-11-13 01:51:12 -0600

    child adding to both fact and rule upon infer

--- hw4/Blocks Code/kb_constructor.py
+++ hw4/Blocks Code/kb_constructor.py
@@ -26,11 +26,11 @@ Compare a fact against a rule, and infer new facts/rules
 def infer_from_fact(fact, rule, fb, rb):
     bindings = facts_and_rules.match(rule.lhs[0], fact)
 
-     # if valid match b/w rule and fact
+    # if valid match b/w rule and fact
     if bindings != False:
         if (rule.type == "Assert"):
 
-             # if rule lhs is a single pattern, it can only infer a new fact
+            # if rule lhs is a single pattern, it can only infer a new fact
             if (len(rule.lhs) == 1):
                 new_fact = facts_and_rules.instantiate(rule.rhs.full, bindings)
 
@@ -38,29 +38,29 @@ def infer_from_fact(fact, rule, fb, rb):
                 new_fact = facts_and_rules.statement(new_fact)
                 return new_fact
 
-             # if rule lhs is a conjunction of patterns, it can only infer a new rule
+            # if rule lhs is a conjunction of patterns, it can only infer a new rule
             else:
                 new_rule_lhs = [facts_and_rules.instantiate(x.full, bindings) for x in rule.lhs[1:]]
                 new_rule_rhs = facts_and_rules.instantiate(rule.rhs.full, bindings)
                 new_rule = facts_and_rules.rule(new_rule_lhs, new_rule_rhs)
 
-                 # return new rule, even if it's already in rb
+                # return new rule, even if it's already in rb
                 return new_rule
 
         elif (rule.type == "Retract"):
             invalidated_fact = facts_and_rules.instantiate(rule.rhs.full, bindings)
             
-             # create invalidated_fact as a statement object
+            # create invalidated_fact as a statement object
             invalidated_fact = facts_and_rules.statement(invalidated_fact)
             
-             # populate its children and return the invalidated fact
+            # populate its children and return the invalidated fact
             for f in fb:
                 if (f.full == invalidated_fact.full):
                     invalidated_fact.facts = f.facts
                     invalidated_fact.rules = f.rules
                     return invalidated_fact
 
-             # if no children, simply return the invalidated fact
+            # if no children, simply return the invalidated fact
             return invalidated_fact
 
         else:
@@ -89,43 +89,67 @@ def expand_existing(fb, rb):
 
 
 '''
-Assert a new fact (or rule) into KB; also assert other facts/rules INFERRED from comparing this new fact (or rule)
-with the existing rules (or facts)
+Assert a new fact (or rule) into KB; also assert other facts/rules INFERRED from comparing this new fact 
+(or rule) with the existing rules (or facts)
 '''
 def assert_fr(obj, fb, rb):
-     # if obj is a fact
+    # if obj is a fact
     if (type(obj) is facts_and_rules.statement):
 
-         # add new fact only if it's not already in fb
+        # add new fact only if it's not already in fb
         if (obj.full not in map(lambda x: x.full, fb)):
             fb.append(obj)
             
-             # now infer and add/retract more facts or rules
+            # now infer and add/retract more facts or rules
             for r in rb:
                 inference_yield = infer_from_fact(obj, r, fb, rb)
                 if (inference_yield != False):
+                    
+                    # if the newly added fact triggers a retraction, remove the now invalidated fact
+                    # and all facts/rules that it supports
                     if (r.type == "Retract"):
                         fb, rb = retract_fr(inference_yield, fb, rb)
+
+                    # if the newly added fact triggers an assertion, assert the inferred fact/rule to KB
+                    # but first make that the child of the fact+rule that triggered the assertion
                     elif (r.type == "Assert"):
+                        if (type(inference_yield) is facts_and_rules.statement):
+                            obj.add_fact(inference_yield)
+                            r.add_fact(inference_yield)
+                        elif (type(inference_yield) is facts_and_rules.rule):
+                            obj.add_rule(inference_yield)
+                            r.add_rule(inference_yield)
                         fb, rb = assert_fr(inference_yield, fb, rb)
                     else:
                         raise ValueError("rule type is neither Assert nor Retract")
         return fb, rb
 
-     # else if obj is a rule
+    # else if obj is a rule
     elif (type(obj) is facts_and_rules.rule):
          
-         # add new rule only if it's not already in rb
+        # add new rule only if it's not already in rb
         if (obj.full not in map(lambda x: x.full, rb)):
             rb.append(obj)
 
-             # now infer and add/retract more facts or rules
+            # now infer and add/retract more facts or rules
             for f in fb:
                 inference_yield = infer_from_fact(f, obj, fb, rb)
                 if (inference_yield != False):
+
+                    # if the newly added rule triggers a retraction, remove the now invalidated fact
+                    # and all facts/rules that it supports
                     if (obj.type == "Retract"):
                         fb, rb = retract_fr(inference_yield, fb, rb)
+                    
+                    # if the newly added rule triggers an assertion, assert the inferred fact/rule to KB
+                    # but first make that the child of the rule+fact that triggered the assertion
                     elif (obj.type == "Assert"):
+                        if (type(inference_yield) is facts_and_rules.statement):
+                            obj.add_fact(inference_yield)
+                            f.add_fact(inference_yield)
+                        elif (type(inference_yield) is facts_and_rules.rule):
+                            obj.add_rule(inference_yield)
+                            f.add_rule(inference_yield)
                         fb, rb = assert_fr(inference_yield, fb, rb)
                     else:
                         raise ValueError("rule type of obj is neither Assert nor Retract")
@@ -175,8 +199,8 @@ def retract_fr(obj, fb, rb):
 
 
 '''
-Given a list of patterns, like [['color', '?x', 'blue'], ['inst', 'bigbox', '?w']], return the bindings in KB that
-hold when those patterns are true
+Given a list of patterns, like [['color', '?x', 'blue'], ['inst', 'bigbox', '?w']], return the bindings in KB 
+that hold when those patterns are true
 '''
 def ask(patterns, fb):
     bindings_list = []
@@ -193,7 +217,7 @@ def ask(patterns, fb):
 if __name__ == '__main__':
     facts, rules = read.read_tokenize("statements.txt")
 
-     # populate FB and RB with initial knowledge from file
+    # populate FB and RB with initial knowledge from file
     FB = []
     RB = []
     for f in facts:
@@ -204,20 +228,44 @@ if __name__ == '__main__':
     # infer more knowledge from initial file knowledge
     FB, RB = expand_existing(FB, RB)
     
+    # display everything inferred from initial KB
+    print "Facts and Rules given in and inferred from file:"
     for f in FB:
         print f.pretty()
-    print len(FB)
-    print "----------------------------"
-    
+    print "no. of facts: ", len(FB)
+    print "----------------------------"    
     for r in RB:
         print r.pretty()
-    print len(RB)
+    print "no. of rules: ", len(RB)
     print "----------------------------"
 
-    query = [['color', '?x', 'blue'], ['color', '?y', 'red'], ['flat', '?z'], ['inst', 'bigbox', '?w']]
-    bl = ask(query, FB)
-    print "For Ask query: "
-    print query
-    print "\nthe answers are: "
-    print bl
-    print "----------------------------"
+    # prompt user to ASK or ASSERT
+    prompt_flag = True
+    while (prompt_flag == True):
+        option = input("What do you want to do? (Enter \"ask\" or \"assert\" or \"quit\", with quotes):\n")
+        if (option == "ask"):
+            query = input("Enter the list of patterns to query, like \"((color ?x blue), (flat ?z))\":\n")
+            query = [x.replace(')','').replace('(','').split() for x in query.strip('(').strip(')').split(',')]
+            bl = ask(query, FB)
+            print "For Ask query: ", query
+            print "the answers are: ", bl
+            print "----------------------------"
+        if (option == "assert"):
+            prompt = "Enter the fact or rule to assert into KB, like \"fact: (clear cube3)\",\n" + \
+            "or \"rule: ((married ?x ?y) (love ?x ?y)) -> (happy ?x)\", with quotes:\n"
+            assertion = input(prompt)
+            assertion = make_obj(assertion)
+            FB, RB = assert_fr(assertion, FB, RB)
+            print "Knowledge base after making assertion:"
+            for f in FB:
+                print f.pretty()
+            print "no. of facts: ", len(FB)
+            print "----------------------------"    
+            for r in RB:
+                print r.pretty()
+            print "no. of rules: ", len(RB)
+            print "----------------------------"
+        if (option == "quit"):
+            prompt_flag = False
+
+
